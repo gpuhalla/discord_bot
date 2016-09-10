64fd4b239a0dea336dedeef93ff065e2a12b78ec
Josh Clark <clark.2525@osu.edu>
2016-09-10 15:54:37 -0400

    Dictionary time boizzz

--- bot.py
+++ bot.py
@@ -1,8 +1,45 @@
 import discord
 import asyncio
 
-client = discord.Client()
+async def commandCommend(message):
+	return
 
+async def commandTest(message):
+	counter = 0
+	tmp = await client.send_message(message.channel, 'Calculating messages...')
+	async for log in client.logs_from(message.channel, limit=100):
+		if log.author == message.author:
+			counter += 1
+
+	await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+	
+async def commandPoints(message):
+	return
+
+async def commandRoulette(message):
+	return
+	
+async def commandQuote(message):
+	return
+	
+async def commandCatgirl(message):
+	return
+
+async def commandHusbando(message):
+	return
+
+commands = {
+	'!commend'	: commandCommend,
+	'!test' 	: commandTest,
+	'!points'	: commandPoints,
+	'!roulette' : commandRoulette,
+	'!quote'	: commandQuote,
+	'!catgirl'	: commandCatgirl,
+	'!husbando'	: commandHusbando
+}
+	
+client = discord.Client()
+	
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -12,15 +49,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+	if message.content.startswith('!'):
+		cmd = message.content.partition(' ')[0]
+		if cmd in commands.keys():
+			await commands[cmd](message)
 
