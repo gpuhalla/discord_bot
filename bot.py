ac0c65776957e3be97d44c5084c1c695cdcfc022
Josh Clark <clark.2525@osu.edu>
2016-09-10 16:18:53 -0400

    Okay so they had command support all along

--- bot.py
+++ bot.py
@@ -1,56 +1,40 @@
 import discord
-import asyncio
+from discord.ext import commands
+import random
 
-async def commandCommend(message):
-	return
+bot = commands.Bot(command_prefix='!', description='The official BuckeyeLAN bot')
 
-async def commandTest(message):
-	counter = 0
-	tmp = await client.send_message(message.channel, 'Calculating messages...')
-	async for log in client.logs_from(message.channel, limit=100):
-		if log.author == message.author:
-			counter += 1
-
-	await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-	
-async def commandPoints(message):
-	return
-
-async def commandRoulette(message):
-	return
-	
-async def commandQuote(message):
-	return
-	
-async def commandCatgirl(message):
-	return
-
-async def commandHusbando(message):
-	return
-
-commands = {
-	'!commend'	: commandCommend,
-	'!test' 	: commandTest,
-	'!points'	: commandPoints,
-	'!roulette' : commandRoulette,
-	'!quote'	: commandQuote,
-	'!catgirl'	: commandCatgirl,
-	'!husbando'	: commandHusbando
-}
-	
-client = discord.Client()
-	
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-@client.event
-async def on_message(message):
-	if message.content.startswith('!'):
-		cmd = message.content.partition(' ')[0]
-		if cmd in commands.keys():
-			await commands[cmd](message)
+@bot.command()
+async def commend():
+	return
 
+@bot.command()
+async def test():
+	await bot.say('Test!')
+
+@bot.command()
+async def points():
+	return
+
+@bot.command()
+async def roulette(amount : int):
+	return
+
+@bot.command()
+async def quote():
+	return
+
+@bot.command()
+async def catgirl():
+	return
+
+@bot.command()
+async def husbando():
+	return
