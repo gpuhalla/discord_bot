4ada31de9e2f170b0f6deef61d529925da5230c3
gpuhalla <gpuhalla1@gmail.com>
2016-09-11 22:54:25 -0400

    quote fix 1e100

--- bot.py
+++ bot.py
@@ -50,8 +50,9 @@ async def quote():
 	else:
 		cursor = c.execute('''SELECT * FROM quotes ORDER BY RANDOM() LIMIT 1''')
 		#should pick out the second  and third fields of what the command returns
-		attributor = c.fetchone()[1]
-		quote = c.fetchone()[2]
+		await bot.say(c.fetchall())
+		attributor = c.fetchone()[0]
+		quote = c.fetchone()[1]
 		await bot.say(quote)
 		await bot.say("  -" + attributor)
 	return
