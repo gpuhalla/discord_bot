a837b98750bc0e75950cbd1850c7ab7f6bc29160
Josh Clark <clark.2525@osu.edu>
2016-09-12 20:20:43 -0400

    points v0.00001

--- bot.py
+++ bot.py
@@ -2,19 +2,25 @@ import discord
 from discord.ext import commands
 import random
 import sqlite3
+import time
 
 #Exact filepath may be needed.
 conn = sqlite3.connect('bot_db.sqlite')
 c = conn.cursor()
 
+#Use second cursor for all the points stuff that happens 
+#in the background task to reduce conflicts with other commands
+points_cursor = conn.cursor()
+
 bot = commands.Bot(command_prefix='!', description='The official BuckeyeLAN bot')
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+	print('Logged in as')
+	print(bot.user.name)
+	print(bot.user.id)
+	print('------')
+	bot.loop.create_task(pointsBackgroundTask())
 
 
 def checkTableExists(tableName):
@@ -23,6 +29,24 @@ def checkTableExists(tableName):
 	#Returns the query result. 0 for does not exist. 1 for exists.
 	return c.fetchone()[0]
 	
+
+async def pointsBackgroundTask():
+	if not checkTableExists("Points"):
+		points_cursor.execute("""CREATE TABLE "Points" ("UserID" VARCHAR(20) PRIMARY KEY NOT NULL UNIQUE, "numPoints" INTEGER NOT NULL DEFAULT 0)""")
+	while True:
+		for server in bot.servers:
+			for member in server.members:
+				if member.status == discord.enums.Status.online:
+					await addPoints(member.id, 1)
+		time.sleep(30)
+	
+async def addPoints(userID, numPoints):
+	points_cursor.execute("SELECT * FROM Points WHERE UserID = ?", (str(userID), ))
+	if points_cursor.fetchall() is None:
+		points_cursor.execute("INSERT INTO Points (UserID, numPoints) VALUES (?, 0)", (str(userID), ))
+		conn.commit()
+	points_cursor.execute("UPDATE Points SET numPoints = numPoints + ? WHERE UserID = ?", (int(numPoints), str(userID), ))
+	conn.commit()
 	
 @bot.command()
 async def commend():
@@ -33,10 +57,18 @@ async def commend():
 async def test():
 	await bot.say('Test!')
 
-@bot.command()
-async def points():
-	await bot.say(' has a total of xxx points!')
-	return
+@bot.command(pass_context=True)
+async def points(ctx):
+	userID = ctx.message.author.id
+	name = ctx.message.author.name
+	points_cursor.execute("SELECT numPoints FROM Points WHERE UserID = ?", (str(userID), ))
+	points = points_cursor.fetchone()
+	points_cursor.fetchall()
+	if points is None:
+		await bot.say("{0} has a total of 0 points!".format(str(name)))
+	else:
+		await bot.say("{0} has a total of {1} points!".format(str(name), int(points[0])))
+	
 
 @bot.command()
 async def roulette(amount : int):
@@ -89,4 +121,4 @@ async def shrek():
 async def husbando():
 	return
 
-bot.run('MjI0MjM0MzUzMTcxODkwMTc3.CrYQfA.gA246SiyVTS_SICctEo-7JJKwRU')
+bot.run('MjI0MjM0MzUzMTcxODkwMTc3.CrjL4A.TwmYMflSnCkmz_MSueuSSx2Y6OE')
